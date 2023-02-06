79f677a02e7a8b8722dc27d457f73dd507c795e3
Rostyslav <rossgaluzinskiy@gmail.com>
2023-02-06 22:31:28 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -7,11 +7,14 @@ import SearchForm from 'components/SearchForm';
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
   const [films, setFilms] = useSearchParams();
   const nameFilm = films.get('name') || '';
 
   const handleSubmit = e => {
     e.preventDefault();
+    setMovies([]);
+    setError(null);
     const form = e.target;
     const searchQuery = form.searchName.value;
     setFilms({ name: searchQuery });
@@ -21,12 +24,22 @@ const Movies = () => {
     if (nameFilm === '') {
       return;
     }
-    SearchFilmByName.searchFilm(nameFilm).then(data => setMovies(data.results));
+    SearchFilmByName.searchFilm(nameFilm)
+      .then(data => {
+        if (!data.results.length) {
+          throw new Error();
+        }
+        setMovies(data.results);
+      })
+      .catch(error => {
+        setError({ error });
+      });
   }, [nameFilm]);
 
   return (
     <>
       <h2>Search film</h2>
+      {error && <p>Щось пішло не так. Оновіть сторінку.</p>}
       <SearchForm onSubmit={handleSubmit} />
       {movies && <SearchMovie movies={movies} />}
     </>
