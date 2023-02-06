b5a5d268bca3e5cd6178536def18a03813afc961
Rostyslav <rossgaluzinskiy@gmail.com>
2023-02-06 14:43:58 +0200

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,49 +1,58 @@
 import { useSearchParams, Link, useLocation } from 'react-router-dom';
 import * as SearchFilmByName from 'components/api-service/api';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [films, setFilms] = useSearchParams();
-  const nameFilm = films.get('name') ?? '';
+  const nameFilm = films.get('name') || '';
   const location = useLocation();
-
-  const onChangeQueryString = name => {
-    const nextName = name !== '' ? { name } : {};
-    setFilms(nextName);
-  };
 
   const handleSubmit = e => {
     e.preventDefault();
+    const form = e.target;
+    const searchQuery = form.searchName.value;
+    setFilms({ name: searchQuery });
+  };
+
+  useEffect(() => {
     if (nameFilm === '') {
-      alert('Write something in search');
       return;
     }
+
     SearchFilmByName.searchFilm(nameFilm).then(data => setMovies(data.results));
-  };
+  }, [nameFilm]);
 
   return (
     <>
       <h2>Search film</h2>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={nameFilm}
-          onChange={e => onChangeQueryString(e.target.value)}
-        />
+        <input type="text" name="searchName" />
         <button type="submit">Search</button>
       </form>
 
-      {movies &&
-        movies.map(movie => {
-          return (
-            <li key={movie.id}>
-              <Link to={`${movie.id}`} state={{ from: location }}>
-                {movie.title}
-              </Link>
-            </li>
-          );
-        })}
+      <ul>
+        {movies &&
+          movies
+            .filter(movie =>
+              movie.title.toLowerCase().includes(nameFilm.toLowerCase())
+            )
+            .map(({ id, poster_path, title }) => {
+              return (
+                <li key={id}>
+                  <Link to={`${id}`} state={{ from: location }}>
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                      alt={title}
+                      width="300"
+                    />
+                    {title}
+                  </Link>
+                </li>
+              );
+            })}
+      </ul>
     </>
   );
 };
